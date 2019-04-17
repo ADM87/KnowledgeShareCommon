ecb3f5525e1bc034875f51931a2194530048dffe
TEAMSPACE\amalley <aaron.malley@redspace.com>
2019-04-17 16:21:37 -0300

    Committing initial common project files

---
+++ Assets/Scripts/Factories/AssetFactory.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace KnowledgeShareCommon
+{
+    public static class AssetFactory
+    {
+        public static void CreateSomeCoolAsset(string name)
+        {
+            Debug.Log(string.Format("Creating from cool asset called {0}", name));
+        }
+    }
+}
